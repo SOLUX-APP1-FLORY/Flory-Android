a5443916fe1d8d29996d7a51399f086697557035
gaeulzzang <gaeulzzang@gmail.com>
2024-05-29 16:20:58 +0900

    feat: BottomNavigation 구현

---
+++ app/src/main/java/com/solux/flory/presentation/date/DateFragment.kt
@@ -0,0 +1,18 @@
+package com.solux.flory.presentation.date
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.solux.flory.R
+import com.solux.flory.databinding.FragmentDateBinding
+import com.solux.flory.util.base.BindingFragment
+
+class DateFragment : BindingFragment<FragmentDateBinding>(FragmentDateBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/solux/flory/presentation/main/MainActivity.kt
+++ app/src/main/java/com/solux/flory/presentation/main/MainActivity.kt
@@ -5,7 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.solux.flory.R
 import com.solux.flory.databinding.ActivityMainBinding
+import com.solux.flory.presentation.date.DateFragment
+import com.solux.flory.presentation.gift.GiftFragment
+import com.solux.flory.presentation.home.HomeFragment
+import com.solux.flory.presentation.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,5 +23,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val currentFragment = supportFragmentManager.findFragmentById(binding.fcvMain.id)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fcvMain.id, HomeFragment())
+                .commit()
+        }
+
+        clickBottomNavigation()
+    }
+
+    private fun clickBottomNavigation() {
+        binding.bnvMain.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_date -> {
+                    replaceFragment(DateFragment())
+                    true
+                }
+
+                R.id.menu_gift -> {
+                    replaceFragment(GiftFragment())
+                    true
+                }
+
+                R.id.menu_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fcvMain.id, fragment)
+            .commit()
     }
 }
