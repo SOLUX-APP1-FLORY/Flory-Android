d68c0bc4f59d103c792da9ab720538a0db7ed4f9
gaeulzzang <gaeulzzang@gmail.com>
2024-06-02 21:50:09 +0900

    feat: shadow 구현

--- app/src/main/java/com/solux/flory/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/solux/flory/presentation/profile/ProfileViewModel.kt
@@ -6,19 +6,39 @@ class ProfileViewModel : ViewModel() {
     val mockNeighbors = listOf(
         NeighborInfo(
             profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
-            profileName = "Gaeul"
+            profileName = "가을"
         ),
         NeighborInfo(
             profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
-            profileName = "Gaeul"
+            profileName = "가을"
         ),
         NeighborInfo(
             profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
-            profileName = "Gaeul"
+            profileName = "가을"
         ),
         NeighborInfo(
             profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
-            profileName = "Gaeul"
-        )
+            profileName = "가을"
+        ),
+        NeighborInfo(
+            profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
+            profileName = "가을"
+        ),
+        NeighborInfo(
+            profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
+            profileName = "가을"
+        ),
+        NeighborInfo(
+            profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
+            profileName = "가을"
+        ),
+        NeighborInfo(
+            profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
+            profileName = "가을"
+        ),
+        NeighborInfo(
+            profileImage = "https://avatars.githubusercontent.com/u/91470334?v=4",
+            profileName = "가을"
+        ),
     )
 }
