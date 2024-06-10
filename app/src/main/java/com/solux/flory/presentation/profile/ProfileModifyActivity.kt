4c99f0eba30bb60ffb72bbbc5fcb930bd6ef7a0c
gaeulzzang <gaeulzzang@gmail.com>
2024-06-10 13:21:01 +0900

    refactor: BindingActivity 적용

--- app/src/main/java/com/solux/flory/presentation/profile/ProfileModifyActivity.kt
+++ app/src/main/java/com/solux/flory/presentation/profile/ProfileModifyActivity.kt
@@ -1,21 +1,15 @@
 package com.solux.flory.presentation.profile
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.solux.flory.R
 import com.solux.flory.databinding.ActivityProfileModifyBinding
+import com.solux.flory.util.base.BindingActivity
 
-class ProfileModifyActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityProfileModifyBinding
+class ProfileModifyActivity : BindingActivity<ActivityProfileModifyBinding>({
+    ActivityProfileModifyBinding.inflate(it)
+}) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProfileModifyBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         leftArrowClick()
         maleSelected()
@@ -31,7 +25,7 @@ class ProfileModifyActivity : AppCompatActivity() {
     }
 
     private fun femaleSelected() {
-        with(binding){
+        with(binding) {
             clModifySexFemale.setOnClickListener {
                 clModifySexMale.isSelected = false
                 clModifySexFemale.isSelected = true
