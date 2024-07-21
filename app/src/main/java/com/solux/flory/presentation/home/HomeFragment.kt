4fef0e105b41423c838f58279259eecb9e7867e0
김세희 <lolry0207@sookmyung.ac.kr>
2024-07-22 00:25:31 +0900

    feat: 메인 페이지 background 변경 로직 구현

--- app/src/main/java/com/solux/flory/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/solux/flory/presentation/home/HomeFragment.kt
@@ -4,20 +4,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.solux.flory.R
+import com.solux.flory.databinding.FragmentHomeBinding
+import com.solux.flory.presentation.home.HomeViewModel
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout for this fragment using data binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        // Initialize ViewModel
+        viewModel = ViewModelProvider(requireActivity()).get(HomeViewModel::class.java)
+
+        // Set the ViewModel in the binding
+        binding.viewModel = viewModel
+
+        // Observe LiveData for background change
+        viewModel.rangeValue.observe(viewLifecycleOwner) { rangeValue ->
+            updateBackgroundForRange(rangeValue)
+        }
+
+        // Return the root view of the fragment
+        return binding.root
+    }
+    private fun updateBackgroundForRange(value: Int) {
+        val backgroundResource = getBackgroundResourceForRange(value)
+        binding.clHomeFragment.setBackgroundResource(backgroundResource)
+    }
+
+    private fun getBackgroundResourceForRange(value: Int): Int {
+        return when {
+            value <= 5 -> R.drawable.background_home1
+            value <= 11 -> R.drawable.background_home2
+            value <= 17 -> R.drawable.background_home3
+            value <= 23 -> R.drawable.background_home4
+            else -> R.drawable.background_home5
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setViewModelData()
+    }
+
+    private fun setViewModelData() {
+        viewModel.setRangeValue(18)
     }
 }
