1ed461e9deeb79c4ff368f3377bbe0738e0b240c
김세희 <lolry0207@sookmyung.ac.kr>
2024-07-31 00:57:19 +0900

    feat: 상단 진행 바 추가

--- app/src/main/java/com/solux/flory/presentation/gift/confirm/PresentDetailFragment.kt
+++ app/src/main/java/com/solux/flory/presentation/gift/confirm/PresentDetailFragment.kt
@@ -8,6 +8,9 @@ import coil.load
 import com.solux.flory.R
 import com.solux.flory.databinding.FragmentGiftDetailBinding
 import com.solux.flory.util.base.BindingFragment
+import com.solux.flory.util.context.stringOf
+import com.solux.flory.util.fragment.stringOf
+import com.solux.flory.util.setupToolbarClickListener
 
 class PresentDetailFragment : BindingFragment<FragmentGiftDetailBinding>(FragmentGiftDetailBinding::inflate) {
 
@@ -29,6 +32,12 @@ class PresentDetailFragment : BindingFragment<FragmentGiftDetailBinding>(Fragmen
             binding.ivGiftDetailImage.load(it.imageUrl)
             binding.tvGiftDetailText.text = it.message
             binding.tvGiftDetailSender.text = it.sender
+        }
+    }
+
+    private fun initToolbar() {
+        with(binding.toolbarGiftDetail) {
+            setupToolbarClickListener(ibToolbarIcon)
         }
     }
 
